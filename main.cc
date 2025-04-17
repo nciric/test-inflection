801c8a6739f8553927befa99098be84b165d4666
Nebojsa Ciric <nciric@gmail.com>
2025-04-17 15:21:11 -0700

    Working first inflection API call

---
+++ main.cc
@@ -0,0 +1,37 @@
+/*
+ * clang++ -L/usr/local/lib -licuuc -licui18n -linflection -std=c++17 main.cc -o main
+ *
+ * Assuming that both inflection and icu4c are installed in /usr/local/lib
+ */
+
+#include <iostream>
+#include <string>
+
+#include <inflection/lang/features/LanguageGrammarFeatures.hpp>
+#include <unicode/platform.h>
+#include <unicode/uconfig.h>
+#include <unicode/unistr.h>
+
+using namespace icu;
+using namespace std;
+
+int main()
+{
+    inflection::util::ULocale locale("sr");
+    string utf8;
+    auto categories(inflection::lang::features::LanguageGrammarFeatures::getLanguageGrammarFeatures(locale).getCategories());
+    for (const auto &[categoryName, category] : categories)
+    {
+        utf8.clear();
+        UnicodeString name(categoryName);
+        cout << name.toUTF8String<string>(utf8).c_str() << endl;
+        for (const auto &value : category.getValues())
+        {
+            utf8.clear();
+            UnicodeString value2(value);
+            cout << "    " << value2.toUTF8String<string>(utf8).c_str() << endl;
+        }
+    }
+
+    return 0;
+}
